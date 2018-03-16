29c33c6deb7e5e7e949f8f4939f2114b4d8142d8
Akshay Nanavati <akshay.nanavati1@gmail.com>
2018-03-16 16:34:50 -0700

    Add option to run with clang

--- scripts/run_bm.sh
+++ scripts/run_bm.sh
@@ -2,6 +2,21 @@
 
 set -e
 
-g++-7 -std=c++17 -O3 -lbenchmark benchmarks/$1.cpp
-./a.out --benchmark_format=csv > results/$1_results.csv
-./scripts/normalize_csv.py results/$1_results.csv
+if [ "$2" == "clang" ]
+then
+COMPILER='/usr/local/opt/llvm/bin/clang++'
+EXTRA_FLAGS="-L$HOME/clang-lib/ -lbenchmark -I$HOME/clang-lib/"
+OUT_FILE="results/$1_results_clang.csv"
+BIN_NAME="$1_clang.out"
+else
+COMPILER='/usr/local/bin/g++-7'
+EXTRA_FLAGS='-lbenchmark'
+OUT_FILE="results/$1_results_gcc.csv"
+BIN_NAME="$1_gcc.out"
+fi
+
+echo "Compilng with $COMPILER..."
+
+$COMPILER $EXTRA_FLAGS -std=c++17 -O3 -o$BIN_NAME benchmarks/$1.cpp
+./$BIN_NAME --benchmark_format=csv > $OUT_FILE
+./scripts/normalize_csv.py $OUT_FILE
